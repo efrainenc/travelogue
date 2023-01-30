9ccb2ea177993def576043d2a19a5dedf96d1c73
unknown <efraine387@gmail.com>
2023-01-30 10:21:08 -0500

    Django allAuth setup

--- travel_project/travel_project/settings.py
+++ travel_project/travel_project/settings.py
@@ -60,17 +60,16 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-# Provider specific settings
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
-        # For each OAuth based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        },
+        'OAUTH_PKCE_ENABLED': True,  # this is different
     }
 }
 
